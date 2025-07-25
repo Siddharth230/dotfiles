de654233b12bc8b09b64647ce3597ef55a6522e6
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-25 14:01:28 +0530

    Modified Configurations

--- waybar/.config/waybar/scripts/get_weather.sh
+++ waybar/.config/waybar/scripts/get_weather.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
+LOCATION="Pune"
+
 for i in {1..5}; do
-  text=$(curl -s "https://wttr.in/$1?format=1")
-  if [[ $? == 0 ]]; then
+  text=$(curl -s "https://wttr.in/${LOCATION}?format=1")
+  if [[ $? == 0 && "$text" != *"Unknown location"* ]]; then
     text=$(echo "$text" | sed -E "s/\s+/ /g")
-    tooltip=$(curl -s "https://wttr.in/$1?format=4")
-    if [[ $? == 0 ]]; then
+    tooltip=$(curl -s "https://wttr.in/${LOCATION}?format=4")
+    if [[ $? == 0 && "$tooltip" != *"Unknown location"* ]]; then
       tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
       echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
       exit
