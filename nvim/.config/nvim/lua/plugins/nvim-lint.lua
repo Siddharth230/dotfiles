7c800dfe12e554bcfeb387803c811e9197187865
Siddharth230 <siddharthlokhande04@gmail.com>
2026-04-03 18:55:57 +0530

    Updated nvim-lint

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,17 +1,8 @@
 return {
   "mfussenegger/nvim-lint",
-  event = "BufWritePost",
-  config = function()
-    require("lint").linters_by_ft =
-      {
-        python = {
-          "pylint",
-        },
-      }, vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        pattern = { "*.py" },
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
-  end,
+  opts = {
+    linters_by_ft = {
+      python = { "ruff" },
+    },
+  },
 }
